a55d260b949db32744767f202bcb8feea7013f06
Arthur Williams <arthurw125@gmail.com>
2021-05-20 02:07:22 +0000

    updated esbn

--- .ipynb_checkpoints/dist3-checkpoint.py
+++ .ipynb_checkpoints/dist3-checkpoint.py
@@ -27,67 +27,81 @@ seq_len = 9
 
 n_shapes = 100
 m_holdout = 0
+
+# Randomly assign objects to training or test set
+all_shapes = np.arange(n_shapes)
+np.random.shuffle(all_shapes)
+if m_holdout > 0:
+	train_shapes = all_shapes[m_holdout:]
+	test_shapes = all_shapes[:m_holdout]
+else:
+	train_shapes = all_shapes
+	test_shapes = all_shapes
+
+
 train_set_size = 10000
 test_set_size = 10000
+train_proportion = 0.95
 # Method for calculating number of combinations
 def n_comb(n, r):
 	return int(math.factorial(n) / (math.factorial(r) * math.factorial(n-r)))
 
 # Task generator
-def create_task(args, train_shapes, test_shapes):
-
-	log.info('n_shapes = ' + str(args.n_shapes) + '...')
-	log.info('m_holdout = ' + str(args.m_holdout) + '...')
+def create_task():
+	global n_shapes,m_holdout,all_shapes,train_shapes,test_shapes,train_set_size,test_set_size,train_proportion
+	#global train_set_size
+	log.info('n_shapes = ' + str(n_shapes) + '...')
+	log.info('m_holdout = ' + str(m_holdout) + '...')
 	# If m = 0, training and test sets are drawn from same set of shapes
-	if args.m_holdout == 0:
+	if m_holdout == 0:
 		# Total number of possible trials
-		shapes_avail = args.n_shapes
+		shapes_avail = n_shapes
 		n_row_comb = n_comb(shapes_avail, 3)
 		n_metaperm = (3*2*1)**2
 		total_trials = n_metaperm * n_row_comb
 		log.info('Total possible trials = ' + str(total_trials) + '...')
-		if args.train_set_size + args.test_set_size > total_trials:
+		if train_set_size + test_set_size > total_trials:
 			# Proportion of training set size vs. test set size
-			train_proportion = args.train_proportion
+			#train_proportion = args.train_proportion
 			test_proportion = 1 - train_proportion
 			# Create training/test set sizes
-			log.info('Desired training set size (' + str(args.train_set_size) + ') and test set size (' + str(args.test_set_size) + ') combined are larger than total number of possible trials for this task (' + str(total_trials) + ')...')
-			args.train_set_size = np.round(train_proportion * total_trials).astype(np.int)
-			log.info('Changing training set size to ' + str(args.train_set_size) + '...')
-			args.test_set_size = np.round(test_proportion * total_trials).astype(np.int)
-			log.info('Changing test set size to ' + str(args.test_set_size) + '...')
+			log.info('Desired training set size (' + str(train_set_size) + ') and test set size (' + str(test_set_size) + ') combined are larger than total number of possible trials for this task (' + str(total_trials) + ')...')
+			train_set_size = np.round(train_proportion * total_trials).astype(np.int)
+			log.info('Changing training set size to ' + str(train_set_size) + '...')
+			test_set_size = np.round(test_proportion * total_trials).astype(np.int)
+			log.info('Changing test set size to ' + str(test_set_size) + '...')
 		else:
-			log.info('Training set size = ' + str(args.train_set_size) + '...')
-			log.info('Test set size = ' + str(args.test_set_size) + '...')
+			log.info('Training set size = ' + str(train_set_size) + '...')
+			log.info('Test set size = ' + str(test_set_size) + '...')
 	else:
 		# Total number of possible training trials
-		shapes_avail = args.n_shapes - args.m_holdout
+		shapes_avail = n_shapes - m_holdout
 		n_row_comb = n_comb(shapes_avail, 3)
 		n_metaperm = (3*2*1)**2
 		total_trials = n_metaperm * n_row_comb
 		log.info('Total possible training trials = ' + str(total_trials) + '...')
-		if args.train_set_size > total_trials:
-			log.info('Desired training set size (' + str(args.train_set_size) + ') is larger than total number of possible training trials for this task (' + str(total_trials) + ')...')
+		if train_set_size > total_trials:
+			log.info('Desired training set size (' + str(train_set_size) + ') is larger than total number of possible training trials for this task (' + str(total_trials) + ')...')
 			log.info('Changing training set size to ' + str(total_trials) + '...')
-			args.train_set_size = total_trials
+			train_set_size = total_trials
 		else:
-			log.info('Training set size = ' + str(args.train_set_size) + '...')
+			log.info('Training set size = ' + str(train_set_size) + '...')
 		# Total number of possible training trials
-		shapes_avail = args.n_shapes - (args.n_shapes - args.m_holdout)
+		shapes_avail = n_shapes - (n_shapes - m_holdout)
 		n_row_comb = n_comb(shapes_avail, 3)
 		n_metaperm = (3*2*1)**2
 		total_trials = n_metaperm * n_row_comb
 		log.info('Total possible test trials = ' + str(total_trials) + '...')
-		if args.test_set_size > total_trials:
-			log.info('Desired test set size (' + str(args.test_set_size) + ') is larger than total number of possible test trials for this task (' + str(total_trials) + ')...')
+		if test_set_size > total_trials:
+			log.info('Desired test set size (' + str(test_set_size) + ') is larger than total number of possible test trials for this task (' + str(total_trials) + ')...')
 			log.info('Changing test set size to ' + str(total_trials) + '...')
-			args.test_set_size = total_trials
+			test_set_size = total_trials
 		else:
-			log.info('Test set size = ' + str(args.test_set_size) + '...')
+			log.info('Test set size = ' + str(test_set_size) + '...')
 
 	# Generate complete matrix problems
 	# If m = 0, training and test sets are drawn from same set of shapes
-	if args.m_holdout == 0:
+	if m_holdout == 0:
 		# Create all possible combinations
 		all_row_comb = builtins.list(combinations(train_shapes, 3))
 		# Create all possible permutations for each combination
@@ -101,8 +115,8 @@ def create_task(args, train_shapes, test_shapes):
 		random.shuffle(all_trials)
 		all_trials = np.array(all_trials)
 		# Split trials for train and test sets
-		trials_train = all_trials[:args.train_set_size,:,:]
-		trials_test = all_trials[args.train_set_size:(args.train_set_size+args.test_set_size),:,:]
+		trials_train = all_trials[:train_set_size,:,:]
+		trials_test = all_trials[train_set_size:(train_set_size+test_set_size),:,:]
 	# Otherwise, training and test sets are completely disjoint (in terms of the shapes that are used), and can be generated separately
 	else:
 		# Training trials
@@ -119,7 +133,7 @@ def create_task(args, train_shapes, test_shapes):
 		random.shuffle(all_trials)
 		all_trials = np.array(all_trials)
 		# Split trials for train and test sets
-		trials_train = all_trials[:args.train_set_size,:,:]
+		trials_train = all_trials[:train_set_size,:,:]
 		# Test trials
 		# Create all possible combinations
 		all_row_comb = builtins.list(combinations(test_shapes, 3))
@@ -134,7 +148,7 @@ def create_task(args, train_shapes, test_shapes):
 		random.shuffle(all_trials)
 		all_trials = np.array(all_trials)
 		# Split trials for train and test sets
-		trials_test = all_trials[:args.test_set_size,:,:]
+		trials_test = all_trials[:test_set_size,:,:]
 
 	# Generate multiple-choice options
 	# Training set
@@ -192,4 +206,4 @@ def create_task(args, train_shapes, test_shapes):
 	train_set = {'seq_ind': np.array(train_MC_seq), 'y': np.array(train_MC_targ)}
 	test_set = {'seq_ind': np.array(test_MC_seq), 'y': np.array(test_MC_targ)}
 
-	return args, train_set, test_set
+	return train_set, test_set
